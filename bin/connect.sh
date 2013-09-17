22ee93bb2950f038e27b3f14d77777471c33d194
George Ornbo <george@shapeshed.com>
2013-09-17 22:11:56 +0100

    [feature] add ii connect script

---
+++ bin/connect.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+: "${ircdir:=$HOME/irc}"
+: "${nick:=$USER}"
+
+# server info functions
+freenode() {
+	server='irc.freenode.net'
+	channels="#archlinux #node.js"
+}
+
+campfire() {
+	server='localhost'
+	channels="#bede_bingo_team"
+}
+
+# these match the functions above
+networks="freenode"
+
+# some privacy please, thanks
+chmod 700 "$ircdir"
+chmod 600 "$ircdir"/*/ident &>/dev/null
+
+for network in $networks; do
+	unset server channels port
+	"$network" # set the appropriate vars
+
+	while true; do
+		# cleanup
+		rm -f "$ircdir/$server/in"
+
+		# connect to netwrok -- password is set through the env var synonym to the network name
+		# iim -i "$ircdir" -n "$nick" -k "$network" -s "$server" -p "${port:-6667}" &
+		ii -i "$ircdir" -n "$nick" -k "$network" -s "$server" -p "${port:-6667}" &
+		pid="$!"
+
+		# wait for the connection
+		while ! test -p "$ircdir/$server/in"; do sleep .3; done
+
+		# auth to services
+		if [ -e "$ircdir/$server/ident" ]
+		then printf "/j nickserv identify %s\n" "$(cat "$ircdir/$server/ident")" > "$ircdir/$server/in"
+		fi && rm -f "$ircdir/$server/nickserv/out" # clean that up - ident passwd is in there
+
+		# join channels
+		printf "/j %s\n" $channels > "$ircdir/$server/in"
+
+		# if connection is lost reconnect
+		wait "$pid"
+	done &
+done
+
