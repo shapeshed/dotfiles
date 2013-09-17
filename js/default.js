162fa825c711e4c645d3d9c4296cca44efc2fcb8
George Ornbo <george@shapeshed.com>
2013-09-17 22:17:56 +0100

    [refactor] remove js folder

--- js/default.js
+++
@@ -1,5 +0,0 @@
-
-
-//$('head').append('<script language="javascript" type="text/javascript" src="http://lab.arc90.com/experiments/readability/js/readability.js"/>');
-//$('head').append('<link rel="stylesheet" href="http://lab.arc90.com/experiments/readability/css/readability.css" type="text/css" />');
-
--- js/www.espncricinfo.com.js
+++
@@ -1,3 +0,0 @@
-$('#ciHomeMastContainer').hide();
-$('.seriesSpncr').hide();
-$('.pnl320T, .pnl320M, .pnl320B, .pnl320T, .pnl320M, .pnl320B').hide();
