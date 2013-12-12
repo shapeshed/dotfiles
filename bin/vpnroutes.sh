eb64b668ebf4aa50343d347a96b881110ee6c636
George Ornbo <george@shapeshed.com>
2013-12-12 08:16:02 +0000

    [feature] add more work vpn routes

--- bin/vpnroutes.sh
+++ bin/vpnroutes.sh
@@ -9,3 +9,7 @@ sudo ip route add 10.128.0.10 via 192.168.3.254
 # teamcity.bedegaming.net 
 sudo ip route add 168.63.24.231 via 192.168.3.254
 
+# dev01-xxx.bedegaming.net
+sudo ip route add 168.63.31.10 via 192.168.3.254
+
+
