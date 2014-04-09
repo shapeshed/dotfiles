245d9dee0c46f1342cd371e6d01a72d1127882bf
George Ornbo <george@shapeshed.com>
2014-04-09 14:49:53 +0100

    [feature] add more vpn routes

--- bin/vpnroutes.sh
+++ bin/vpnroutes.sh
@@ -12,4 +12,10 @@ sudo ip route add 168.63.24.231 via 192.168.3.254
 # dev01-xxx.bedegaming.net
 sudo ip route add 168.63.31.10 via 192.168.3.254
 
+# qa01-xxx.bedegaming.net
+sudo ip route add 168.63.31.26 via 192.168.3.254
 
+# bingostars.com
+sudo ip route add 168.63.23.138 via 192.168.3.254
+
+sudo ip route add 168.63.23.125 via 192.168.3.254
