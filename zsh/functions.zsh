b4957a23919bf09819f445599c0881585e99899f
George Ornbo <george@shapeshed.com>
2017-04-07 07:53:51 +0100

    Remove password generator function
    Using pass now

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -3,13 +3,3 @@ function mkcd {
     mkdir -p "$*"
     cd "$*"
 }
-
-function genpasswd {
-  local l=12 # default password lenght
-  if [ "$#" != "0" -a "$1" -gt 0 ]
-  then
-    l=$1
-  fi
-  LC_CTYPE=C tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
-}
-
