7846363c7a44d653ed397093f9c03461c493de0b
George Ornbo <george@shapeshed.com>
2017-04-07 08:19:49 +0100

    Add fg and gr zsh functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,32 @@
-# Make directory and cd inside it
+# mkcd - make a directory and change to it
+#
+# This makes a directory with parents and changes into it
+# mkdir /some/path/that/does/not/exist
+# pwd
+# /some/path/that/does/not/exist
+#
 function mkcd {
     mkdir -p "$*"
     cd "$*"
+}
+
+# fg - make zsh behave like bash
+#
+# By default zsh has the following behaviour
+# fg %1
+#
+# This function makes fg behave like the bash builtin
+# fg 1
+#
+fg() {
+  if [[ $# -eq 1 && $1 = - ]]; then
+    builtin fg %-
+  else
+    builtin fg %"$@"
+  fi
+}
+
+# gr - search recursively with grep
+function gr {
+    grep -R "$*" .
 }
