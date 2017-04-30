ccd35d8b490a71a79b3516521c0a70e6ac9c6f18
George Ornbo <george@shapeshed.com>
2017-04-30 09:46:19 +0100

    Remove unused vpn scripts

--- bin/startvpn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/bin/systemctl start openswan
-sleep 2
-/usr/bin/systemctl start xl2tpd 
-/usr/sbin/ipsec auto --up L2TP-PSK                        
-/bin/echo "c vpn-connection" > /var/run/xl2tpd/l2tp-control     
-sleep 4 
