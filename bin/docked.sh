27a5aa9b8cbf6831dbd673d7bdf0bc875884bff1
George Ornbo <george@shapeshed.com>
2018-11-27 10:15:56 +0000

    Add docked and undocked scripts

---
+++ bin/docked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x2648 --rotate normal --output DP1 --mode 3840x2160 --pos 1920x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output DP2 --off
---
+++ bin/undocked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
