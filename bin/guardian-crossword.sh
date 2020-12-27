79ed688d666c65b1bd466e5212973d234efa5691
George Ornbo <george@shapeshed.com>
2020-12-27 16:44:08 +0000

    Add script to send crosswords

---
+++ bin/guardian-crossword.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+DATE=`date +%Y%m%d`
+URL="https://crosswords-static.guim.co.uk"
+QUICK="gdn.quick.$DATE.pdf"
+CRYPTIC="gdn.cryptic.$DATE.pdf"
+
+# Send to Remarkable
+
+# Quick crosswords published Mon-Sat
+if [ $(date +%u) -le 6 ] 
+then
+    p2r -p crosswords $URL/$QUICK
+fi
+# Cryptic crosswords published Mon-Fri
+if [ $(date +%u) -le 5 ] 
+then
+    p2r -p crosswords $URL/$CRYPTIC
+fi
+
