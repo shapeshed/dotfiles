c770c450134ab992d226cb6899c61406509ef8d3
George Ornbo <george@shapeshed.com>
2020-12-29 10:01:39 +0000

    Add ./local/bin to path (pip)

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
 [ -d ~/bin ] && export PATH=~/bin:$PATH
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$(ruby -r rubygems -e "puts Gem.user_dir")/bin:$HOME/go/bin:/usr/local/heroku/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$(ruby -r rubygems -e "puts Gem.user_dir")/bin:$HOME/go/bin:/usr/local/heroku/bin:$HOME/.local/bin:$PATH"
