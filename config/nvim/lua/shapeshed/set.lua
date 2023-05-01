74aff76c73911de043a1988d7eff79daf3957281
George Ornbo <george@shapeshed.com>
2023-05-01 20:58:40 +0100

    Add some mappings

--- config/nvim/lua/shapeshed/set.lua
+++ config/nvim/lua/shapeshed/set.lua
@@ -1,5 +1,13 @@
-vim.opt.nu          = true
-vim.opt.tabstop 	= 4
+vim.opt.nu = true
+vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth 	= 4
-vim.opt.expandtab 	= true
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.g.mapleader = ","
+
+vim.o.termguicolors = true
+vim.cmd.colorscheme("catppuccin")
+
+vim.keymap.set("n", "<C-h>", ":bprevious<CR>")
+vim.keymap.set("n", "<C-l>", ":bnext<CR>")
