8c08eecd608461cfae132f3e5663e2550da8473e
George Ornbo <george@shapeshed.com>
2023-05-08 12:00:19 +0100

    Keep more history and share it

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,3 +1,10 @@
 HISTFILE=~/.zsh_history
-HISTSIZE=1000
-SAVEHIST=1000
+HISTSIZE=50000
+SAVEHIST=10000
+
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
