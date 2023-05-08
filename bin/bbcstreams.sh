62f2dbce600e4744640eda239e28d60c5d3a274a
George Ornbo <george@shapeshed.com>
2023-05-08 13:02:13 +0100

    Update after shfmt

--- bin/bbcstreams.sh
+++ bin/bbcstreams.sh
@@ -13,13 +13,11 @@ radios["BBC Radio 5 Live"]="http://www.bbc.co.uk/radio/listen/live/r5l_aaclca.pl
 radios["BBC Radio 5 Sports Extra"]="http://www.bbc.co.uk/radio/listen/live/r5lsp_aaclca.pls"
 radios["BBC 6 Music"]="http://www.bbc.co.uk/radio/listen/live/r6_aaclca.pls"
 
-
-for k in "${!radios[@]}"
-do
+for k in "${!radios[@]}"; do
   filepath="${playlistdir}/${k}.m3u"
   rm -f "$filepath"
-  echo "#EXTM3U" >> "$filepath"
+  echo "#EXTM3U" >>"$filepath"
   pls=${radios[$k]}
-  echo "#EXTINF:-1, BBC - $k" >> "$filepath"
-  curl -s $pls | grep File1 | sed 's/File1=\(.*\)/\1/' >> "$filepath"
+  echo "#EXTINF:-1, BBC - $k" >>"$filepath"
+  curl -s $pls | grep File1 | sed 's/File1=\(.*\)/\1/' >>"$filepath"
 done
