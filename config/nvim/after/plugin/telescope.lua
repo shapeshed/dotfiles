96e047ed7b5200999fa1006a376808003ba03fe2
George Ornbo <george@shapeshed.com>
2023-05-13 07:22:29 +0100

    Refactor telescope config

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,16 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<C-p>", builtin.git_files, { silent = true, desc = "Search git files" })
-vim.keymap.set("n", "?", builtin.keymaps, { silent = true, desc = "See keymaps" })
+local actions = require("telescope.actions")
+require("telescope").setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+      },
+      n = {
+        ["q"] = actions.close,
+      },
+    },
+  },
+})
