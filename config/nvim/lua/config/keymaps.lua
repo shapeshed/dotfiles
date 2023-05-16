607e43de808388f9696d2e62c8a956c05736b37a
George Ornbo <george@shapeshed.com>
2023-05-16 13:55:21 +0100

    Add gitsigns and completion

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,10 @@
 local builtin = require("telescope.builtin")
-local lazy = require("lazy")
 local dap = require("dap")
 local dapui = require("dapui")
 local helpers = require("utils.helpers")
 
--- Mappings ordered under <leader> in a logical namespace
--- for use with which-key (my memory fades..)
+-- Mappings ordered under <LEADER> in a logical namespace
+-- for use with which-key (my memory fades..🕱)
 -- * b - Buffers
 -- * d - Debugger
 -- * f - Find
@@ -14,330 +13,125 @@ local helpers = require("utils.helpers")
 -- * p - Packages
 
 local mappings = {
-  -- Top level useful commands
-  { "n", "<leader><leader>", "<C-^>", "Open last buffer" },
-  { "n", "<leader>w", ":write<CR>", "Save" },
-  { "n", "<leader>q", ":q<CR>", "Quit" },
-  { "n", "<leader>e", ":Lexplore %:p:h<CR>", "Open File Explorer" },
-  { "n", "<leader>t", ":TroubleToggle<CR>", "Toggle Trouble" },
-  { "n", "<C-l>", ":bnext<CR>", "Next buffer" },
-  { "n", "<C-h>", ":bprevious<CR>", "Previous buffer" },
-  { "n", "<C-x>", ":bdelete<CR>", "Close buffer" },
+    -- Top level useful commands
+    { "n", "<LEADER><LEADER>", "<C-^>",                   "Open last buffer" },
+    { "n", "<LEADER>w",        "<cmd>write<CR>",          "Save" },
+    { "n", "<LEADER>q",        "<cmd>q<CR>",              "Quit" },
+    { "n", "<LEADER>e",        "<cmd>Lexplore %:p:h<CR>", "Open File Explorer" },
+    { "n", "<LEADER>t",        "<cmd>TroubleToggle<CR>",  "Toggle Trouble" },
+    { "n", "<C-l>",            "<cmd>bnext<CR>",          "Next buffer" },
+    { "n", "<C-h>",            "<cmd>bprevious<CR>",      "Previous buffer" },
+    { "n", "<C-x>",            "<cmd>bdelete<CR>",        "Close buffer" },
 
-  -- Comment/Uncomment in insert and visual mode
-  {
-    "n",
-    "<leader>/",
-    function()
-      require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-    end,
-    "Comment line",
-  },
-  {
-    "v",
-    "<leader>/",
-    function()
-      require("Comment.api").toggle.linewise(vim.fn.visualmode())
-    end,
-    "Comment line",
-  },
+    -- Comment/Uncomment in insert and visual mode
+    {
+        "n",
+        "<LEADER>/",
+        function()
+            require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
+        end,
+        "Comment line",
+    },
+    {
+        "v",
+        "<LEADER>/",
+        function()
+            require("Comment.api").toggle.linewise(vim.fn.visualmode())
+        end,
+        "Comment line",
+    },
 
-  -- Buffers
-  { "n", "<leader>bc", ":bdelete<CR>", "Close buffer" },
-  { "n", "<leader>bn", ":bnext<CR>", "Next buffer" },
-  { "n", "<leader>bp", ":bprevious<CR>", "Previous buffer" },
+    -- Buffers
+    { "n", "<LEADER>bc", "<cmd>bdelete<CR>",             "Close buffer" },
+    { "n", "<LEADER>bn", "<cmd>bnext<CR>",               "Next buffer" },
+    { "n", "<LEADER>bp", "<cmd>bprevious<CR>",           "Previous buffer" },
 
-  -- Debugger (D) namespace
-  {
-    "n",
-    "<leader>dc",
-    function()
-      dap.continue()
-    end,
-    "Continue",
-  },
-  {
-    "n",
-    "<leader>db",
-    function()
-      dap.toggle_breakpoint()
-    end,
-    "Toggle Breakpoint",
-  },
-  {
-    "n",
-    "<leader>dB",
-    function()
-      vim.ui.input({ prompt = "Condition: " }, function(expr)
-        if expr then
-          dap.toggle_breakpoint(expr)
-        end
-      end)
-    end,
-    "Conditional breakpoint",
-  },
-  {
-    "n",
-    "<leader>di",
-    function()
-      dap.step_into()
-    end,
-    "Step Into",
-  },
-  {
-    "n",
-    "<leader>dO",
-    function()
-      dap.step_out()
-    end,
-    "Step Out",
-  },
-  {
-    "n",
-    "<leader>do",
-    function()
-      dap.step_over()
-    end,
-    "Step Over",
-  },
-  {
-    "n",
-    "<leader>dp",
-    function()
-      dap.pause()
-    end,
-    "Pause Debugger",
-  },
-  {
-    "n",
-    "<leader>dq",
-    function()
-      dap.close()
-    end,
-    "Close Session",
-  },
-  {
-    "n",
-    "<leader>dQ",
-    function()
-      dap.close()
-    end,
-    "Terminate Session",
-  },
-  {
-    "n",
-    "<leader>dR",
-    function()
-      dap.restart_frame()
-    end,
-    "Restart Debugger",
-  },
-  {
-    "n",
-    "<leader>dE",
-    function()
-      vim.ui.input({ prompt = "Expression: " }, function(expr)
-        if expr then
-          dapui.eval(expr)
-        end
-      end)
-    end,
-    "Evaluate Input",
-  },
-  {
-    "v",
-    "<leader>dE",
-    function()
-      dapui.eval()
-    end,
-    "Evaluate Input",
-  },
-  {
-    "n",
-    "<leader>du",
-    function()
-      dapui.toggle()
-    end,
-    "Toggle Debugger UI",
-  },
-  {
-    "n",
-    "<leader>dh",
-    function()
-      dapui.hover()
-    end,
-    "Debugger Hover",
-  },
+    -- Debugger (D) namespace
+    { "n", "<LEADER>dc", "<cmd>DapContinue<CR>",         "Continue" },
+    { "n", "<LEADER>db", "<cmd>DapToggleBreakpoint<CR>", "Toggle breakpoint" },
+    {
+        "n",
+        "<LEADER>dB",
+        function()
+            vim.ui.input({ prompt = "Condition: " }, function(expr)
+                if expr then
+                    dap.toggle_breakpoint(expr)
+                end
+            end)
+        end,
+        "Conditional breakpoint",
+    },
+    { "n", "<LEADER>di", "<cmd>DapStepInto<CR>",     "Step into" },
+    { "n", "<LEADER>dO", "<cmd>DapStepOut<CR>",      "Step out" },
+    { "n", "<LEADER>do", "<cmd>DapStepOver<CR>",     "Step over" },
+    { "n", "<LEADER>dq", "<cmd>DapTerminate<CR>",    "Close session" },
+    { "n", "<LEADER>dR", "<cmd>DapRestartFrame<CR>", "Restart debugger" },
+    {
+        "n",
+        "<LEADER>dE",
+        function()
+            vim.ui.input({ prompt = "Expression: " }, function(expr)
+                if expr then
+                    dapui.eval(expr)
+                end
+            end)
+        end,
+        "Evaluate Input",
+    },
+    {
+        "n",
+        "<LEADER>du",
+        function()
+            dapui.toggle()
+        end,
+        "Toggle Debugger UI",
+    },
+    {
+        "n",
+        "<LEADER>dh",
+        function()
+            dapui.hover()
+        end,
+        "Debugger Hover",
+    },
 
-  -- Find (F) namespace
-  {
-    "n",
-    "<leader>f'",
-    function()
-      builtin.marks()
-    end,
-    "Find marks",
-  },
-  {
-    "n",
-    "<leader>fb",
-    function()
-      builtin.buffers()
-    end,
-    "Find buffers",
-  },
-  {
-    "n",
-    "<leader>fc",
-    function()
-      builtin.grep_string()
-    end,
-    "Find word under cursor",
-  },
-  {
-    "n",
-    "<leader>fC",
-    function()
-      builtin.commands()
-    end,
-    "Find commands",
-  },
-  {
-    "n",
-    "<leader>ff",
-    function()
-      builtin.find_files()
-    end,
-    "Find files",
-  },
-  {
-    "n",
-    "<leader>fF",
-    function()
-      builtin.find_files()({ hidden = true, no_ignore = true })
-    end,
-    "Find all files",
-  },
-  {
-    "n",
-    "<leader>fg",
-    function()
-      builtin.live_grep()
-    end,
-    "Find words",
-  },
-  {
-    "n",
-    "<leader>fh",
-    function()
-      builtin.help_tags()
-    end,
-    "Find help",
-  },
-  {
-    "n",
-    "<leader>fm",
-    function()
-      builtin.man_pages()
-    end,
-    "Find man",
-  },
+    -- Find (F) namespace
+    { "n", "<LEADER>f'", "<cmd>Telescope marks<CR>",       "Find marks" },
+    { "n", "<LEADER>fb", "<cmd>Telescope buffers<CR>",     "Find buffers" },
+    { "n", "<LEADER>fb", "<cmd>Telescope grep_string<CR>", "Find word under cursor" },
+    { "n", "<LEADER>fC", "<cmd>Telescope commands<CR>",    "Find commands" },
+    { "n", "<LEADER>ff", "<cmd>Telescope find_files<CR>",  "Find files" },
+    { "n", "<LEADER>ff", "<cmd>Telescope find_files<CR>",  "Find files" },
+    {
+        "n",
+        "<LEADER>fF",
+        function()
+            builtin.find_files()({ hidden = true, no_ignore = true })
+        end,
+        "Find all files",
+    },
+    { "n", "<LEADER>fg", "<cmd>Telescope live_grep<CR>",            "Find words" },
+    { "n", "<LEADER>fh", "<cmd>Telescope help_tags<CR>",            "Find help" },
+    { "n", "<LEADER>fm", "<cmd>Telescope man_pages<CR>",            "Find man" },
 
-  -- LSP (L) namespace
-  { "n", "<leader>li", ":LspInfo<CR>", "LSP Information" },
-  { "n", "<leader>lI", ":NullLsInfo<CR>", "NullLs Information" },
-  {
-    "n",
-    "<leader>ld",
-    function()
-      vim.lsp.buf.definition()
-    end,
-    "Go to Definition",
-  },
-  {
-    "n",
-    "<leader>lD",
-    function()
-      builtin.diagnostics()
-    end,
-    "Search diagnostics",
-  },
-  {
-    "n",
-    "<leader>ls",
-    function()
-      builtin.lsp_document_symbols()
-    end,
-    "Search symbols",
-  },
-  {
-    "n",
-    "<leader>lk",
-    function()
-      vim.lsp.buf.hover()
-    end,
-    "Hover information",
-  },
-  {
-    "n",
-    "<leader>lr",
-    function()
-      vim.lsp.buf.rename()
-    end,
-    "Rename string",
-  },
-  {
-    "n",
-    "<leader>lS",
-    function()
-      vim.lsp.buf.signature_help()
-    end,
-    "Signature help",
-  },
+    -- LSP (L) namespace
+    { "n", "<LEADER>li", "<CMD>LspInfo<CR>",                        "LSP information" },
+    { "n", "<LEADER>lI", "<CMD>NullLsInfo<CR>",                     "NullLs information" },
+    { "n", "<LEADER>ld", vim.lsp.buf.definition,                    "Go to definition" },
+    { "n", "<LEADER>lD", "<CMD>Telescope diagnostics<CR>",          "Search diagnostics" },
+    { "n", "<LEADER>ls", "<CMD>Telescope lsp_document_symbols<CR>", "Search symbols" },
+    { "n", "<LEADER>lk", vim.lsp.buf.hover,                         "Hover information" },
+    { "n", "<LEADER>lr", vim.lsp.buf.rename,                        "Rename string" },
+    { "n", "<LEADER>lS", vim.lsp.buf.signature_help,                "Signature help" },
 
-  -- Packages (P) namespace
-  {
-    "n",
-    "<leader>pi",
-    function()
-      lazy.install()
-    end,
-    "Plugins install",
-  },
-  {
-    "n",
-    "<leader>ps",
-    function()
-      lazy.home()
-    end,
-    "Plugins Status",
-  },
-  {
-    "n",
-    "<leader>pS",
-    function()
-      lazy.sync()
-    end,
-    "Plugins Sync",
-  },
-  {
-    "n",
-    "<leader>pu",
-    function()
-      lazy.check()
-    end,
-    "Plugins Check Updates",
-  },
-  {
-    "n",
-    "<leader>pU",
-    function()
-      lazy.update()
-    end,
-    "Plugins Update",
-  },
+    -- Packages (P) namespace
+    { "n", "<LEADER>pi", "<CMD>Lazy install<CR>",                   "Plugins install" },
+    { "n", "<LEADER>ps", "<CMD>Lazy home<CR>",                      "Plugins status" },
+    { "n", "<LEADER>pS", "<CMD>Lazy sync<CR>",                      "Plugins sync" },
+    { "n", "<LEADER>pu", "<CMD>Lazy check<CR>",                     "Plugins check updates" },
+    { "n", "<LEADER>pU", "<CMD>Lazy update<CR>",                    "Plugins update" },
 }
 
 for _, v in pairs(mappings) do
-  helpers.setMapping(v[1], v[2], v[3], v[4])
+    helpers.setMapping(v[1], v[2], v[3], v[4])
 end
---
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping(
+            cmp.mapping.confirm({
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+            }),
+            { "i", "c" }
+          ),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        experimental = {
+          native_menu = false,
+          ghost_text = true,
+        },
+      }
+    end,
+  },
+}
