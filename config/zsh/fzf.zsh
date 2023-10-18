5940d5af3fa605afd8779ece781ea3695c99511d
George Ornbo <george@shapeshed.com>
2023-10-18 11:32:10 +0100

    Set colors for fzf

---
+++ config/zsh/fzf.zsh
@@ -0,0 +1,4 @@
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
