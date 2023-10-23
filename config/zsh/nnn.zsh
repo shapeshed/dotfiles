2d91eb26876c0216cd6da030804d41f9d8cfc574
George Ornbo <george@shapeshed.com>
2023-10-23 08:27:15 +0100

    Add nnn config

---
+++ config/zsh/nnn.zsh
@@ -0,0 +1,8 @@
+# This second option relies on you're terminal using the catppuccin theme and well use true catppuccin colors:
+BLK="03" CHR="03" DIR="04" EXE="02" REG="07" HARDLINK="05" SYMLINK="05" MISSING="08" ORPHAN="01" FIFO="06" SOCK="03" UNKNOWN="01"
+
+# Export Context Colors
+export NNN_COLORS="#04020301;4231"
+
+# Finally Export the set file colors ( Both options require this)
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$UNKNOWN"
