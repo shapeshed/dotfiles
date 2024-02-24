8c5b0e9c5b83ccd58ab7e75fbfa0a28e7329d9bf
George Ornbo <george@shapeshed.com>
2024-02-24 17:25:16 +0000

    Reference ohmyzsh plugin for fzf

--- config/zsh/fzf.zsh
+++ config/zsh/fzf.zsh
@@ -2,3 +2,6 @@ export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+source $HOME/src/github.com/ohmyzsh/ohmyzsh/plugins/fzf/fzf.plugin.zsh
+
