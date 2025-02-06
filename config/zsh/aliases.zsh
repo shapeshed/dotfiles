e033da7e8c9c354d1ba429af63fb5ba2eae8e5ad
George Ornbo <george@shapeshed.com>
2025-02-06 14:46:56 +0000

    Misc updates

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -19,12 +19,12 @@ alias castget='castget -C ~/.config/castget/castgetrc'
 alias goaccess='goaccess -p ~/.config/goaccess/goaccessrc'
 
 # Radio
-alias playbbcr1="mpv 'http://as-hls-ww-live.akamaized.net/pool_904/live/ww/bbc_radio_one/bbc_radio_one.isml/bbc_radio_one-audio%3d96000.norewind.m3u8'"
-alias playbbcr4="mpv 'http://as-hls-ww-live.akamaized.net/pool_904/live/ww/bbc_radio_fourfm/bbc_radio_fourfm.isml/bbc_radio_fourfm-audio%3d96000.norewind.m3u8'"
-alias playbbcr5l="mpv 'http://as-hls-ww-live.akamaized.net/pool_904/live/ww/bbc_radio_five_live/bbc_radio_five_live.isml/bbc_radio_five_live-audio%3d96000.norewind.m3u8'"
-alias playbbcr5lsx="mpv 'http://as-hls-uk-live.akamaized.net/pool_904/live/uk/bbc_radio_five_live_sports_extra/bbc_radio_five_live_sports_extra.isml/bbc_radio_five_live_sports_extra-audio%3d96000.norewind.m3u8'"
-alias playbbcr6="mpv 'http://as-hls-ww-live.akamaized.net/pool_904/live/ww/bbc_6music/bbc_6music.isml/bbc_6music-audio%3d96000.norewind.m3u8'"
-alias playbbcrnorfolk="mpv 'http://as-hls-ww-live.akamaized.net/pool_904/live/ww/bbc_radio_norfolk/bbc_radio_norfolk.isml/bbc_radio_norfolk-audio%3d96000.norewind.m3u8'"
+alias playbbcr1="mpv 'https://as-hls-uk-live.akamaized.net/pool_01505109/live/uk/bbc_radio_one/bbc_radio_one.isml/bbc_radio_one-audio%3d320000.norewind.m3u8'"
+alias playbbcr4="mpv 'https://as-hls-uk-live.akamaized.net/pool_55057080/live/uk/bbc_radio_fourfm/bbc_radio_fourfm.isml/bbc_radio_fourfm-audio%3d320000.norewind.m3u8'"
+alias playbbcr5l="mpv 'https://as-hls-uk-live.akamaized.net/pool_89021708/live/uk/bbc_radio_five_live/bbc_radio_five_live.isml/bbc_radio_five_live-audio%3d320000.norewind.m3u8'"
+alias playbbcr5lsx="mpv 'http://as-hls-uk-live.akamaized.net/pool_01505109/live/uk/bbc_radio_five_live_sports_extra/bbc_radio_five_live_sports_extra.isml/bbc_radio_five_live_sports_extra-audio%3d96000.norewind.m3u8'"
+alias playbbcr6="mpv 'https://as-hls-uk-live.akamaized.net/pool_47700285/live/uk/bbc_radio_five_live_sports_extra/bbc_radio_five_live_sports_extra.isml/bbc_radio_five_live_sports_extra-audio%3d320000.norewind.m3u8'"
+alias playbbcrnorfolk="mpv 'https://as-hls-uk-live.akamaized.net/pool_61510571/live/uk/bbc_radio_norfolk/bbc_radio_norfolk.isml/bbc_radio_norfolk-audio%3d320000.norewind.m3u8'"
 alias playtalksport2="mpv 'http://radio.talksport.com/stream2?awparams=platform:ts-web&amsparams=playerid:ts-web;'"
 alias playtalksport="mpv 'http://radio.talksport.com/stream?aisGetOriginalStream=true'"
 
